90b64d360ffd37528c6486dacc399306f9575578
Nafis <nafis.shah@gmail.com>
2020-07-18 14:38:04 -0400

    fail test 2

--- src/tests/counter.test.ts
+++ src/tests/counter.test.ts
@@ -1,7 +1,7 @@
 import {clickDown,clickUp} from '../site/components/counter'
 
 test('Test clickDown(0)', () => {
-    expect(clickDown(0)).toBe(-11111);
+    expect(clickDown(0)).toBe(-1111);
 });
 
 test('Test clickUp(0)', () => {
